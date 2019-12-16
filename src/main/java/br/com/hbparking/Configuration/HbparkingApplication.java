483974adb33b74c47fb0c7e19a0b7920474467bd
rubenskleinschmidtjr@gmail.com <pacoloco09>
2019-12-16 14:16:48 -0300

    Initial commit

---
+++ src/main/java/br/com/hbparking/Configuration/HbparkingApplication.java
@@ -0,0 +1,13 @@
+package br.com.hbparking.Configuration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HbparkingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HbparkingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/hbparking/Configuration/HbparkingApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.hbparking.Configuration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HbparkingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

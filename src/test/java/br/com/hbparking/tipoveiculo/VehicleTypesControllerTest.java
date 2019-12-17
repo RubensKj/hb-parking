9d549acd2cbbef7518980c5dbc2dbcf6b4862a6a
rubenskleinschmidtjr@gmail.com <pacoloco09>
2019-12-17 14:27:38 -0300

    PAD-427 tipo de veiculo com test

--- src/test/java/br/com/hbparking/tipoveiculo/VehicleTypesControllerTest.java
+++ src/test/java/br/com/hbparking/tipoveiculo/VehicleTypesControllerTest.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.ArrayList;
 import java.util.Arrays;

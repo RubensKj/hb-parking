9805af5cb91b2455d5cd599d96f70eb884383ce4
Carlos Eduardo Lopes <carlos.lopes@hbsis.com.br>
2019-12-18 11:38:23 -0300

    Cleanup & Organization

--- src/main/java/br/com/hbparking/marcas/IMarcaRepository.java
+++ src/main/java/br/com/hbparking/marcas/IMarcaRepository.java
@@ -3,18 +3,13 @@ package br.com.hbparking.marcas;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface IMarcaRepository extends JpaRepository<Marca, Long> {
 
-    //@Query(value = "select * from marcas where tipo_veiculo like :tipo ", nativeQuery = true)
-    //List<Marca> findMarcaBytipoVeiculo(@Param("tipo") String tipo);
+
     Page<Marca> findAllBytipoVeiculo(TipoVeiculoEnum tipoVeiculo, Pageable pageable);
 
 
--- src/main/java/br/com/hbparking/marcas/MarcaRest.java
+++ src/main/java/br/com/hbparking/marcas/MarcaRest.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletResponse;
-import java.util.List;
 
 @CrossOrigin(origins = {"http://localhost:4200", "http://192.168.33.216:4200", "https://192.168.33.216:4200"})
 @RestController

b51ff864b6bad0899ce56b443b0b0b69d5861491
rubenskleinschmidtjr@gmail.com <pacoloco09>
2019-12-27 09:19:34 -0300

    Default Travis CI

--- src/main/java/br/com/hbparking/marcas/MarcaDTO.java
+++ src/main/java/br/com/hbparking/marcas/MarcaDTO.java
@@ -1,30 +1,50 @@
 package br.com.hbparking.marcas;
 
-
-import lombok.*;
-
-@NoArgsConstructor
-@Data
-@AllArgsConstructor
 public class MarcaDTO {
 
     private Long id;
     private TipoVeiculoEnum tipoVeiculo;
     private String nome;
 
+    public MarcaDTO() {
+    }
+
+    public MarcaDTO(Long id, TipoVeiculoEnum tipoVeiculo, String nome) {
+        this.id = id;
+        this.tipoVeiculo = tipoVeiculo;
+        this.nome = nome;
+    }
 
     public MarcaDTO(TipoVeiculoEnum tipoVeiculo, String nome) {
         this.tipoVeiculo = tipoVeiculo;
         this.nome = nome;
     }
 
-    public static MarcaDTO of(Marca marca){
+    public static MarcaDTO of(Marca marca) {
         return new MarcaDTO(
                 marca.getId(),
                 marca.getTipoVeiculo(),
                 marca.getNome()
-                );
+        );
     }
 
+    public Long getId() {
+        return id;
+    }
 
+    public TipoVeiculoEnum getTipoVeiculo() {
+        return tipoVeiculo;
+    }
+
+    public void setTipoVeiculo(TipoVeiculoEnum tipoVeiculo) {
+        this.tipoVeiculo = tipoVeiculo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 }

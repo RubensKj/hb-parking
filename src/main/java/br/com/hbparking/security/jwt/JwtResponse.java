bddf781d2410a27ca7a077fc45428075a94bb053
efraim.ferreira <efraimthomaz@hotmail.com>
2019-12-27 12:56:09 -0300

    Adicionado autorização ao JwtResponse para ser passado ao front juntamente ao token

--- src/main/java/br/com/hbparking/security/jwt/JwtResponse.java
+++ src/main/java/br/com/hbparking/security/jwt/JwtResponse.java
@@ -1,12 +1,19 @@
 package br.com.hbparking.security.jwt;
 
+import org.springframework.security.core.GrantedAuthority;
+
+import java.util.Collection;
+
 public class JwtResponse {
 
     private String type = "Bearer";
     private String token;
+    private Collection<? extends GrantedAuthority> authorities;
 
-    public JwtResponse(String token) {
+    public JwtResponse(String token, Collection<? extends GrantedAuthority> authorities) {
         this.token = token;
+        this.authorities = authorities;
+
     }
 
     public String getType() {
@@ -23,5 +30,13 @@ public class JwtResponse {
 
     public void setToken(String token) {
         this.token = token;
+    }
+
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    public void setAuthorities(Collection<? extends GrantedAuthority> authorities) {
+        this.authorities = authorities;
     }
 }

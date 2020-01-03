fd987faa8194e484245e9678510665ab56bbe8ec
rubenskleinschmidtjr@gmail.com <pacoloco09>
2020-01-03 10:22:12 -0300

    Adicionando response status para o front conseguir pegar essa informação

--- src/main/java/br/com/hbparking/vehicleException/ContentDispositionException.java
+++ src/main/java/br/com/hbparking/vehicleException/ContentDispositionException.java
@@ -1,7 +1,11 @@
 package br.com.hbparking.vehicleException;
 
-public class ContentDispositionException extends Exception{
-    public ContentDispositionException(String message){
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.CONFLICT)
+public class ContentDispositionException extends Exception {
+    public ContentDispositionException(String message) {
         super(message);
     }
 }

079d8d62801ebb1b9bd9dc684af3125edecf11ba
rubenskleinschmidtjr@gmail.com <pacoloco09>
2020-01-09 15:23:39 -0300

    Adding response status to exception

--- src/main/java/br/com/hbparking/vagaInfo/PeriodoAlreadyExistsException.java
+++ src/main/java/br/com/hbparking/vagaInfo/PeriodoAlreadyExistsException.java
@@ -1,5 +1,9 @@
 package br.com.hbparking.vagaInfo;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.FOUND)
 public class PeriodoAlreadyExistsException extends Exception {
     public PeriodoAlreadyExistsException(String message) {
         super(message);
